e8d19806ada3975d6ea53ac4b119f1ab98e47407
LulalaBoss <katyhatesnake@gmail.com>
2014-01-08 14:42:33 -0800

    Adding the prototype base class: Sim

---
+++ Sim.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import java.io.IOException;
+
+public class Sim extends GameCore
+{
+  public static void main(String[] args)
+  {
+    new Sim().run();
+  }
+  
+  public void update(long elapsedTime)
+  {
+  }
+  
+  public void draw(Graphics2D g)
+  {
+  }
+  
+}
